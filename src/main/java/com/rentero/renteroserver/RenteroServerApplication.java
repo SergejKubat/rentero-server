8d6c048b532439ec91fa7051e5e785e6ead6430e
Sergej <sergej.kubat18@gmail.com>
2021-06-09 17:22:34 +0200

    First commit

---
+++ src/main/java/com/rentero/renteroserver/RenteroServerApplication.java
@@ -0,0 +1,13 @@
+package com.rentero.renteroserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenteroServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenteroServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rentero/renteroserver/RenteroServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentero.renteroserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenteroServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
